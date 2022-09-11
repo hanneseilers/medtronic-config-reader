c49f1e1800bd0de18b1aa2ed660976df43fdc6e8
Hannes Eilers <hanneseilers@gmx.de>
2022-09-11 16:45:16 +0200

    Getting real basal volumes from data.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@
 # medtronic insulin pump.
 # The data must be shared with CareLink portal
 # and exported as csv data.
+import getopt
 import os
-import sys, getopt
-from subroutines.messageboxes import *
+import sys
+import statistics
+import collections
+
 from subroutines.functions import *
+from subroutines.messageboxes import *
 
 version = "0.0.1a"
 
@@ -32,9 +36,15 @@ def process(csv_file=None):
     key_time = "Time"
     key_bolus_number = "Bolus Number"
     key_bolus_volume = "Bolus Volume Selected (U)"
+    key_basal_rate = "Basal Rate (U/h)"
+    key_bolus_volume_delivered = "Bolus Volume Delivered (U)"
+    key_bolus_source = "Bolus Source"
+
+    value_bolus_source_auto_basal = "CLOSED_LOOP_AUTO_BASAL"
 
     unit_blood_glucose = "mg/dl"
     unit_insulin = "I.E."
+    unit_insulin_per_hour = unit_insulin + "/h"
 
     # process data file
     if csv_file:
@@ -44,12 +54,64 @@ def process(csv_file=None):
         data_sets = filter_data_sets(data_sets, keys_to_filter=[
             key_date,
             key_time,
-            key_bolus_number,
-            key_bolus_volume])
+            key_bolus_volume_delivered,
+            key_bolus_source])
+
+        # filter for basal rate
+        basal_rate = get_basal_rate(
+            data_sets=data_sets,
+            key_time=key_time,
+            key_bolus_delivered=key_bolus_volume_delivered,
+            key_bolus_source=key_bolus_source,
+            value_bolus_auto_basal=value_bolus_source_auto_basal)
+        for time in basal_rate:
+            print("{:8}{:.2f} {}".format(time, basal_rate[time], unit_insulin_per_hour))
+
+
+def get_basal_rate(data_sets, key_time, key_bolus_delivered, key_bolus_source, value_bolus_auto_basal):
+    timeslots = {}
+    if isinstance(data_sets, list):
+
+        # get all basal rates
         for entry in data_sets:
-            print(entry[key_date] + " " + entry[key_time] + ": " + entry[key_bolus_number]
-                  + " " + unit_blood_glucose)
-            print("\t" + entry[key_bolus_volume] + " " + unit_insulin)
+
+            if key_time in entry \
+                    and key_bolus_delivered in entry \
+                    and key_bolus_source in entry\
+                    and value_bolus_auto_basal in entry[key_bolus_source]:
+                # TODO: Filter for dates
+                time = entry[key_time]
+                basal_rate = float(str(entry[key_bolus_delivered]).replace(',', '.'))
+
+                # check for valid basel rate
+                if basal_rate > 0:
+                    # get timeslot
+                    time = str(time).split(':')
+                    if not len(time) > 1:
+                        break
+
+                    h = int(time[0])
+                    m = int(time[1])
+
+                    if m <= 15:
+                        m = "00"
+                    elif 15 < m <= 45:
+                        m = "30"
+                    else:
+                        m = "00"
+                        h += 1
+
+                    time = "{:02d}:{}".format(h, m)
+                    if time not in timeslots:
+                        timeslots[time] = []
+                    timeslots[time].append(basal_rate)
+
+        # calculate basal_rate
+        for time in timeslots:
+            data = timeslots[time]
+            timeslots[time] = statistics.median(data)
+
+        return collections.OrderedDict(sorted(timeslots.items()))
 
 
 # ---- MAIN ----
